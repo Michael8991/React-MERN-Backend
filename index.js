978ac44e251f26ce044a62e48dcac807699f110e
Michael Esequiel Rodríguez Iranzo <michael2002982@gmail.com>
2025-03-28 08:24:13 +0100

    Arreglo en rutas

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
 });
